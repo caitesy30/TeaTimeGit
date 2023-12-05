998ce80225aab40fa056528f94b06f3c29a42b3a
caitesy <caitesy30@gmail.com>
2023-12-05 16:36:54 +0800

    Initial commit

---
+++ TeaTimeDemo/TeaTimeDemo/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeaTimeDemo.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id {get; set;}
+        [Required(ErrorMessage = "類別名稱未填寫")]
+        [MaxLength(30)]
+        [DisplayName("類別名稱")]
+        public string Name { get; set;}
+
+        [Required(ErrorMessage = "顯示順序未填寫")]
+        [DisplayName("顯示順序")]
+        [Range(1, 100,ErrorMessage ="輸入範圍應該要在1-100之間")]
+        public int DisplayOrder { get; set;}
+
+    }
+}
+
---
+++ TeaTimeDemo/TeaTimeDemo/data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeaTimeDemo.Models;
+
+namespace TeaTimeDemo.data
+{
+ 
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "茶飲", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "水果茶", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "咖啡", DisplayOrder = 3 });
+        }
+    }
+
+}
